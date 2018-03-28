21dd6b6926e40117d07fd9080c3e15251b344fcd
Bin Xia <binxi@microsoft.com>
2018-03-28 01:31:56 +0000

    Provision Azure Stack Key Vault

--- pkg/services/keyvault/catalog.go
+++ pkg/services/keyvault/catalog.go
@@ -47,5 +47,48 @@ func (m *module) GetCatalog() (service.Catalog, error) {
 				},
 			}),
 		),
+		service.NewService(
+			&service.ServiceProperties{
+				ID:          "835104e5-417c-421a-8f48-18d3a8ec7071",
+				Name:        "azure-stack-keyvault",
+				Description: "Azure Stack Key Vault (Experimental)",
+				Metadata: &service.ServiceMetadata{
+					DisplayName: "Azure Stack Key Vault",
+					ImageURL: "https://azure.microsoft.com/svghandler/key-vault/" +
+						"?width=200",
+					LongDescription: "Safeguard cryptographic keys and other secrets " +
+						"used by cloud apps and services (Experimental)",
+					DocumentationURL: "https://docs.microsoft.com/en-us/azure/azure-stack/user/azure-stack-kv-intro",
+					SupportURL:       "https://azure.microsoft.com/en-us/support/",
+				},
+				Bindable: true,
+				Tags:     []string{"AzureStackCloud", "Key", "Vault"},
+			},
+			m.serviceManager,
+			service.NewPlan(&service.PlanProperties{
+				ID:          "a77a3568-e29b-4841-80cb-1e7fbf7143db",
+				Name:        "standard",
+				Description: "Standard Tier",
+				Free:        false,
+				Extended: map[string]interface{}{
+					"vaultSku": "Standard",
+				},
+				Metadata: &service.ServicePlanMetadata{
+					DisplayName: "Standard Tier",
+				},
+			}),
+			service.NewPlan(&service.PlanProperties{
+				ID:          "f0e341cf-d3e0-4a58-90ce-9e238d2c1bfc",
+				Name:        "premium",
+				Description: "Premium Tier",
+				Free:        false,
+				Extended: map[string]interface{}{
+					"vaultSku": "Premium",
+				},
+				Metadata: &service.ServicePlanMetadata{
+					DisplayName: "Premium Tier",
+				},
+			}),
+		),
 	}), nil
 }
